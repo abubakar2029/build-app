1ffb9848556223e3e2de62072a8e264a9026b097
Abu Bakar <abubakarzafar2029@gmail.com>
2024-02-01 17:45:32 +0500

    build complete

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ let app = Express();
 
 
 
-app.use(Express.static())
-app.use(Express.static("./build"));
+app.use(Express.json())
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+// app.get("/",(req , res)=>{
+//     res.send("Server returned")
+// })
 
-
-
-
-
-
+// app.get('/', function(req, res) {
+//         res.sendFile(path.join(__dirname, "index.html"));
+//     })
+    
+    
+    
+    
+    
+    app.use(Express.static("./build"));
+    
 
 app.listen(7080, () => {
     console.log(`server listening on ${7080}`);
